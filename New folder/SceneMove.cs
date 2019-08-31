7f837faa7c605cb645de611d9687930a7a6c87f0
unknown <fthe4308@gmail.com>
2019-08-31 22:20:30 +0700

    menambah file

---
+++ New folder/SceneMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMove : MonoBehaviour
+{
+    public void TitleMenu()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+    public void LevelSelect()
+    {
+        SceneManager.LoadScene("LevelSelection");
+    }
+
+    public void FirstLevel()
+    {
+        SceneManager.LoadScene("LevelSatu");
+    }
+
+    public void SecondLevel()
+    {
+        SceneManager.LoadScene("LevelDua");
+    }
+
+    public void Story1()
+    {
+        SceneManager.LoadScene("Story1");
+    }
+
+    public void Story2()
+    {
+        SceneManager.LoadScene("Story2");
+    }
+
+    public void Maintain()
+    {
+        SceneManager.LoadScene("UnderMaintain");
+    }
+}
