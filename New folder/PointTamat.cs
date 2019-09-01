dc573361fedfa111ba19d24ee04db5a3929e8fa4
unknown <fthe4308@gmail.com>
2019-09-01 09:09:46 +0700

    mengupdate 'new folder'

---
+++ New folder/PointTamat.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointTamat : MonoBehaviour
+{
+    Text Point;
+
+    private void Start()
+    {
+        Point = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        Point.text = ("Point = ") + PointCounter2.score.ToString();
+    }
+}
